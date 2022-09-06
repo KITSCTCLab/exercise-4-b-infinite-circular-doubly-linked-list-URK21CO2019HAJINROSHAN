5f93c1943c5fa0f07813de1ad667df94dbb052ec
URK21CO2019HAJINROSHAN <91783350+URK21CO2019HAJINROSHAN@users.noreply.github.com>
2022-09-06 11:40:19 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,8 +2,8 @@ length_of_circular_linked_list = int(input())
 circular_linked_list = list(map(int,input().strip().split(" ")))
 final_list = [circular_linked_list[i] for i in range(3)]
 for i in circular_linked_list:
-if i not in final_list:
-final_list.append(i)
+  if i not in final_list:
+    final_list.append(i)
 print(len(final_list))
 for i in final_list:
-print(i,end=" ")
+  print(i,end=" ")
